4b13488380ee13bc9db25390f5db9fc0b33e5436
deepak-venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2025-10-17 14:14:28 -0700

    update to cons chapter in scheme

--- schemer-workbook.rkt
+++ schemer-workbook.rkt
@@ -1,4 +1,42 @@
 #lang racket
+(require racket/trace)
+
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                 (member? a (cdr lat)))))))
+
+(define my-member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      ((eq? (car lat) a) #t)
+      (else (member? a (cdr lat))))))
+
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) a) (cdr lat))
+      (else (cons (car lat)
+                  (rember a (cdr lat)))))))
+
+(define rember-wrong
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      ((eq? (car lat) a) #t)
+      (else (rember-wrong a (cdr lat))))))

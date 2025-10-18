6c734983ff7aaf7f5174184339735e0838d228c9
deepak-venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2025-10-18 06:50:25 -0700

    completed cons the magnificent

--- schemer-workbook.rkt
+++ schemer-workbook.rkt
@@ -78,9 +78,31 @@
           ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
           (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))
 
-(trace subst2)
+(define multirember
+  (lambda (a lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) a) (multirember a (cdr lat)))
+          (else (cons (car lat) (multirember a (cdr lat)))))))
 
+(define multiinsertR
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? old (car lat)) (cons (car lat) (cons new (multiinsertR new old (cdr lat)))))
+          (else (cons (car lat) (multiinsertR new old (cdr lat)))))))
 
+(define multiinsertL
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? old (car lat)) (cons new (cons (car lat) (multiinsertL new old (cdr lat)))))
+          (else (cons (car lat) (multiinsertL new old (cdr lat)))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) old) (cons new (multisubst new old (cdr lat))))
+          (else (cons (car lat) (multisubst new old (cdr lat)))))))
+
+(trace multisubst)
 
 
 
